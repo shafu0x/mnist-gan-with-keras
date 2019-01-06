bc5e91716b9f011dbe0d1cb2ecbaeaca6847316c
Sharif Elfouly <selfouly@gmail.com>
2019-01-06 13:15:58 +0100

    simplified methods

--- discriminator.py
+++ discriminator.py
@@ -23,8 +23,8 @@ class Discriminator:
     def __init__(self, batch_size=128, epochs=2):
         """Initializes a Sequential NN with the keras Sequential object.
         It takes as input one image from the MNIST dataset (shape=(None, 784))
-        and returns one of 1 if it thinks its from the dataset and 0 if doesn't
-        think so.
+        and returns [0,1] if it thinks its from the MNIST dataset and [0,1] if
+        doesn't think so.
 
         Args:
             batch_size (int, optional): Batch size while training. Defaults to
@@ -61,29 +61,43 @@ class Discriminator:
 
         self.model.fit(X, y, epochs=self.epochs, batch_size=self.batch_size, verbose=verbosity)
 
-    def prediction(self, X):
+    def predict(self, X):
         """Predicts if image is from the MNIST dataset or not.
 
         Args:
             X (np.array): Image (Shape=(None, 784)).
 
         Returns:
-            is_from_mnist (int): 1 if true and 0 otherwise.
+            pred_y (np.array): Array of predictions.
+        """
+        pred_y = self.model.predict(X)
+
+        return pred_y
+
+    def is_from_mnist(self, X):
+        """Predicts if image is from the MNIST dataset or not.
+
+        Args:
+            X (np.array): Image (Shape=(1, 784)).
+
+        Returns:
+            is_from_mnist (bool): 1 if true and 0 otherwise.
         """
         # otherwise keras has problems.
         X = X.reshape(1, INPUT_LAYER)
 
-        prediction = self.model.predict(X)
+        prediction = self.predict(X)
 
         if np.argmax(prediction) == 0:
             is_from_mnist = 0
         else:
             is_from_mnist = 1
 
-        return is_from_mnist
+        return bool(is_from_mnist)
 
     def save(self, name):
         """Saves the model.
+
         Args:
             name (str): Name of the model.
         """
@@ -93,6 +107,7 @@ class Discriminator:
 
     def eval(self, X, y):
         """Returns the accuracy of the NN on the test data.
+
         Args:
             X (np.array): Image (Shape=(None, 784)).
             y (np.array): Label (Shape=(None, 2)).
@@ -102,16 +117,11 @@ class Discriminator:
         """
         accuracy = []
 
-        #predictions = self.prediction(X)
-        print(X.shape)
         predictions = self.model.predict(X)
-
-        print(predictions.shape)
 
         for i in range(len(predictions)):
             if np.argmax(y[i]) == np.argmax(predictions[i]):
                 accuracy.append(1)
-
             else:
                 accuracy.append(0)
 
